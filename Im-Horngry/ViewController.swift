7be0d6a6e3548d32c0cca07de4ec94442ebf9434
Timothy Horng <thorng@asu.edu>
2015-07-24 10:22:30 -0700

    buttons

--- Im-Horngry/ViewController.swift
+++ Im-Horngry/ViewController.swift
@@ -11,13 +11,15 @@ import CoreLocation
 
 var locValue: CLLocationCoordinate2D? // Latitude & Longitude value
 var googleSearchWebAddress: String?
-
+var randomVal: String = "" // Random Value from countryDict
+var price = 1
+var radius = 500
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-
+    
     let locManager = CLLocationManager() // Location Variable
     var countryDict = [String: String]() // Country & Adjectival dictionary
-    var randomVal: String = "" // Random Value from countryDict
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,14 +44,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        
-        Network.getGooglePlaces(randomVal){ (response) -> Void in
-            if let places = response {
-                for place in places {
-                    println(place["name"])
-                }
-            }
-        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -57,8 +51,63 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    // setting the price variable
+    @IBAction func price1(sender: UIButton) {
+        price = 1
+        println("you chose $10")
+    }
+    
+    @IBAction func price2(sender: UIButton) {
+        price = 2
+        println("you chose $20")
+
+    }
+    
+    @IBAction func price3(sender: UIButton) {
+        price = 3
+        println("you chose $30")
+
+    }
+    
+    // setting the radius variable
+    @IBAction func feet(sender: UIButton) {
+        radius = 800
+        println("you chose 800m")
+
+    }
+    
+    @IBAction func bike(sender: UIButton) {
+        radius = 5000
+        println("you chose 5000m")
+
+    }
+    
+    @IBAction func car(sender: UIButton) {
+        radius = 20000
+        println("you chose 20000m")
+
+    }
+    
+    @IBAction func goButton(sender: UIButton) {
+        println("LIFTOFFFFFFF")
+        if locValue != nil {
+            Network.getGooglePlaces(randomVal){ (response) -> Void in
+                if let places = response {
+                    for place in places {
+                        println(place["name"])
+                    }
+                }
+            }
+        }
+        else {
+            println("sorry, location not found")
+        }
+    }
+    
+    
+    
+    // Parse .txt file into a dictionary
     func parseTxtToDictionary() {
-        // Parse .txt file into a dictionary
         var arraySeparated = [String]()
         var countryName: String?
         var countryAdjectival: String?
@@ -66,14 +115,16 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let path = NSBundle.mainBundle().pathForResource("countries_of_the_world", ofType: "txt")
         
         if let content = String(contentsOfFile: path!, encoding: NSUTF8StringEncoding, error: nil) {
+            
             var array = content.componentsSeparatedByString("\n")
+            
             for rows in array {
                 
                 //println(rows)
                 arraySeparated = rows.componentsSeparatedByString(",")
                 
                 countryName = arraySeparated[0]
-                countryAdjectival = arraySeparated[1]
+                countryAdjectival = arraySeparated[1].stringByReplacingOccurrencesOfString(" ", withString: "_")
                 
                 countryDict[countryName!] = countryAdjectival
             }
@@ -83,12 +134,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     // This delegate is called, getting the location
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         
-        if let locValue = locValue {
-            var locValue: CLLocationCoordinate2D = manager.location.coordinate
-        }
+        locValue = manager.location.coordinate
+        
     }
-
-
+    
 }
 
 // Requesting from Google Places API
@@ -117,10 +166,13 @@ class Network {
     }
     
     class func getGooglePlaces(place:String, completionHandler: (([NSDictionary]?) -> Void)?) {
-        Network.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?types=food&keyword=" + place + "&key=" + GOOGLE_PLACES_API_KEY + "&location=" + locValue.latitude + "," + locValue.longitude + "&radius=500", completionHandler: { (data) -> Void in
-            if let json = data, places = json["results"] as? [NSDictionary] {
-                completionHandler?(places)
-            }
+        
+        let substring1 = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=\(locValue!.latitude),\(locValue!.longitude)&minprice=0&maxprice=\(price)&radius=\(radius)&opennow=true&types=food&keyword=\(randomVal)&key=" + GOOGLE_PLACES_API_KEY
+        let substring2 = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=cruise&key=" + GOOGLE_PLACES_API_KEY
+        Network.get(substring1, completionHandler: { (data) -> Void in
+                if let json = data, places = json["results"] as? [NSDictionary] {
+                    completionHandler?(places)
+                }
             }, errorHandler: nil)
     }
 }
