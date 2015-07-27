b6d7ef7bbd96433727a2639dee6837f1d7b615e6
Timothy Horng <thorng@asu.edu>
2015-07-27 16:09:53 -0700

    Organization to the Restaurant class, added InfoViewController

---
+++ Im-Horngry/InfoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InfoViewController.swift
+//  Im-Horngry
+//
+//  Created by Timothy Horng on 7/25/15.
+//  Copyright (c) 2015 Timothy Horng. All rights reserved.
+//
+
+import UIKit
+
+let restaurant = Restaurant()
+
+class InfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Im-Horngry/MainViewController.swift
+++ Im-Horngry/MainViewController.swift
@@ -11,15 +11,15 @@ import CoreLocation
 
 var locValue: CLLocationCoordinate2D? // Latitude & Longitude value
 var googleSearchWebAddress: String?
-var randomVal: String = "" // Random Value from countryDict
-var price = 1
-var radius = 500
+var randomCountry: String = "" // Random Value from countryDict
+var price = 0 // price constraint
+var radius = 0 // radius constraint
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+var countryDict = [String: String]() // Country & Adjectival dictionary
+
+class MainViewController: UIViewController, CLLocationManagerDelegate {
     
     let locManager = CLLocationManager() // Location Variable
-    var countryDict = [String: String]() // Country & Adjectival dictionary
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,13 +33,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locManager.startUpdatingLocation() // calls locationManager delegate
         
         // Creates dictionary of Countries and Adjectivals
-        parseTxtToDictionary()
+        Restaurant.parseTxtToDictionary()
         
         // Randomly generate dict value
-        let index: Int = Int(arc4random_uniform(UInt32(countryDict.count)))
-        randomVal = Array(countryDict.values)[index]
+        Restaurant.generateRandomCountry()
         
-        println(randomVal)
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -90,51 +88,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBAction func goButton(sender: UIButton) {
         println("LIFTOFFFFFFF")
-        if locValue != nil {
-            Network.getGooglePlaces(randomVal){ (response) -> Void in
-                if let places = response {
-                    for place in places {
-                        println(place["name"])
-                    }
-                }
-            }
-        }
-        else {
-            println("sorry, location not found")
-        }
-    }
-    
-    
-    
-    // Parse .txt file into a dictionary
-    func parseTxtToDictionary() {
-        var arraySeparated = [String]()
-        var countryName: String?
-        var countryAdjectival: String?
         
-        let path = NSBundle.mainBundle().pathForResource("countries_of_the_world", ofType: "txt")
-        
-        if let content = String(contentsOfFile: path!, encoding: NSUTF8StringEncoding, error: nil) {
-            
-            var array = content.componentsSeparatedByString("\n")
-            
-            for rows in array {
-                
-                //println(rows)
-                arraySeparated = rows.componentsSeparatedByString(",")
-                
-                countryName = arraySeparated[0]
-                countryAdjectival = arraySeparated[1].stringByReplacingOccurrencesOfString(" ", withString: "_")
-                
-                countryDict[countryName!] = countryAdjectival
-            }
-        }
-    }
-    
-    // This delegate is called, getting the location
-    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-        
-        locValue = manager.location.coordinate
+        // sends API request and returns a restaurant
+        Restaurant.returnRestaurant()
         
     }
     
@@ -167,12 +123,77 @@ class Network {
     
     class func getGooglePlaces(place:String, completionHandler: (([NSDictionary]?) -> Void)?) {
         
-        let substring1 = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=\(locValue!.latitude),\(locValue!.longitude)&minprice=0&maxprice=\(price)&radius=\(radius)&opennow=true&types=food&keyword=\(randomVal)&key=" + GOOGLE_PLACES_API_KEY
+        let substring1 = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=\(locValue!.latitude),\(locValue!.longitude)&minprice=0&maxprice=\(price)&radius=\(radius)&opennow=true&types=food&keyword=\(randomCountry)&key=" + GOOGLE_PLACES_API_KEY
         let substring2 = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=cruise&key=" + GOOGLE_PLACES_API_KEY
         Network.get(substring1, completionHandler: { (data) -> Void in
                 if let json = data, places = json["results"] as? [NSDictionary] {
                     completionHandler?(places)
                 }
+                if let json = data, statuses = json["status"] as? [NSDictionary] {
+                    completionHandler?(statuses)
+                }
             }, errorHandler: nil)
     }
+}
+
+
+class Restaurant {
+    
+    // Parse .txt file into a dictionary
+    class func parseTxtToDictionary() {
+        var arraySeparated = [String]()
+        var countryName: String?
+        var countryAdjectival: String?
+        
+        let path = NSBundle.mainBundle().pathForResource("countries_of_the_world", ofType: "txt")
+        
+        if let content = String(contentsOfFile: path!, encoding: NSUTF8StringEncoding, error: nil) {
+            
+            var array = content.componentsSeparatedByString("\n")
+            
+            for rows in array {
+                
+                //println(rows)
+                arraySeparated = rows.componentsSeparatedByString(",")
+                
+                countryName = arraySeparated[0]
+                countryAdjectival = arraySeparated[1].stringByReplacingOccurrencesOfString(" ", withString: "_")
+                
+                countryDict[countryName!] = countryAdjectival
+            }
+        }
+    }
+    
+    
+    // Randomly generate dict value
+    class func generateRandomCountry () {
+        let index: Int = Int(arc4random_uniform(UInt32(countryDict.count)))
+        randomCountry = Array(countryDict.values)[index]
+        
+        println(randomCountry)
+    }
+    
+    class func returnRestaurant() {
+        if locValue != nil {
+            Network.getGooglePlaces(randomCountry){ (response) -> Void in
+                if let places = response {
+                    for place in places {
+                        println(place["name"])
+                        let restaurantName = place["name"]
+                    }
+                }
+            }
+        }
+        else {
+            println("sorry, location not found")
+        }
+    }
+    
+    // This delegate is called, getting the location
+    class func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        
+        locValue = manager.location.coordinate
+        
+    }
+    
 }
