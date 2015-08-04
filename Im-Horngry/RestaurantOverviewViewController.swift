05e68a744b7e57b42f13958974ff190dfc0defd2
Timothy Horng <thorng@asu.edu>
2015-08-04 11:04:19 -0700

    started restaurant overview view controller w/ three restaurants to choose from

---
+++ Im-Horngry/RestaurantOverviewViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RestaurantOverviewViewController.swift
+//  Im-Horngry
+//
+//  Created by Timothy Horng on 8/4/15.
+//  Copyright (c) 2015 Timothy Horng. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantOverviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
