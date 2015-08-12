62404e242438cc80bac013b5cb6219f7a14c2b99
Timothy Horng <thorng@asu.edu>
2015-08-11 23:08:41 -0700

    ui button updates

--- Im-Horngry/MainViewController.swift
+++ Im-Horngry/MainViewController.swift
@@ -16,10 +16,16 @@ var radius = 800 // radius constraint
 
 var countryDict = [String: String]() // Country & Adjectival dictionary
 
+
+
 class MainViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var segmentedControlRadius: UISegmentedControl!
     @IBOutlet weak var segmentedControlPrice: UISegmentedControl!
+    
+    @IBOutlet weak var firstPrice: UIButton!
+    @IBOutlet weak var secondPrice: UIButton!
+    @IBOutlet weak var thirdPrice: UIButton!
     
     let locManager = CLLocationManager() // Location Variable
     
@@ -66,34 +72,71 @@ class MainViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    
-    // Price segmented control
-    @IBAction func price(sender: UISegmentedControl) {
-        switch segmentedControlPrice.selectedSegmentIndex {
-        case 0:
-            priceSelected = 1
-        case 1:
-            priceSelected = 2
-        case 2:
-            priceSelected = 3
-        default:
-            priceSelected = 1
-        }
+    @IBAction func firstPrice(sender: UIButton) {
+        priceSelected = 1
+        
+        sender.backgroundColor = UIColor(red:0.36, green:0.57, blue:1.00, alpha:1.0)
+        sender.setTitleColor(UIColor(red:0.92, green:0.92, blue:0.92, alpha:1.0), forState: UIControlState.Normal)
+        
+        secondPrice.setTitleColor(UIColor(red:0.00, green:0.00, blue:0.00, alpha:1.0), forState: UIControlState.Normal)
+        secondPrice.backgroundColor = UIColor(red:0.92, green:0.92, blue:0.92, alpha:1.0)
+        
+        thirdPrice.setTitleColor(UIColor(red:0.00, green:0.00, blue:0.00, alpha:1.0), forState: UIControlState.Normal)
+        thirdPrice.backgroundColor = UIColor(red:0.92, green:0.92, blue:0.92, alpha:1.0)
+    }
+    @IBAction func secondPrice(sender: UIButton) {
+        priceSelected = 2
+        
+        sender.backgroundColor = UIColor(red:0.36, green:0.57, blue:1.00, alpha:1.0)
+        sender.setTitleColor(UIColor(red:0.92, green:0.92, blue:0.92, alpha:1.0), forState: UIControlState.Normal)
+        
+        firstPrice.setTitleColor(UIColor(red:0.00, green:0.00, blue:0.00, alpha:1.0), forState: UIControlState.Normal)
+        firstPrice.backgroundColor = UIColor(red:0.92, green:0.92, blue:0.92, alpha:1.0)
+        
+        thirdPrice.setTitleColor(UIColor(red:0.00, green:0.00, blue:0.00, alpha:1.0), forState: UIControlState.Normal)
+        thirdPrice.backgroundColor = UIColor(red:0.92, green:0.92, blue:0.92, alpha:1.0)
+
+    }
+    @IBAction func thirdPrice(sender: UIButton) {
+        priceSelected = 3
+        
+        sender.backgroundColor = UIColor(red:0.36, green:0.57, blue:1.00, alpha:1.0)
+        sender.setTitleColor(UIColor(red:0.92, green:0.92, blue:0.92, alpha:1.0), forState: UIControlState.Normal)
+        
+        firstPrice.setTitleColor(UIColor(red:0.00, green:0.00, blue:0.00, alpha:1.0), forState: UIControlState.Normal)
+        firstPrice.backgroundColor = UIColor(red:0.92, green:0.92, blue:0.92, alpha:1.0)
+        
+        secondPrice.setTitleColor(UIColor(red:0.00, green:0.00, blue:0.00, alpha:1.0), forState: UIControlState.Normal)
+        secondPrice.backgroundColor = UIColor(red:0.92, green:0.92, blue:0.92, alpha:1.0)
     }
     
-    // Radius segmented control
-    @IBAction func radiusConstraint(sender: UISegmentedControl) {
-        switch segmentedControlRadius.selectedSegmentIndex {
-        case 0:
-            radius = 800
-        case 1:
-            radius = 5000
-        case 2:
-            radius = 20000
-        default:
-            radius = 800
-        }
-    }
+//    // Price segmented control
+//    @IBAction func price(sender: UISegmentedControl) {
+//        switch segmentedControlPrice.selectedSegmentIndex {
+//        case 0:
+//            priceSelected = 1
+//        case 1:
+//            priceSelected = 2
+//        case 2:
+//            priceSelected = 3
+//        default:
+//            priceSelected = 1
+//        }
+//    }
+//    
+//    // Radius segmented control
+//    @IBAction func radiusConstraint(sender: UISegmentedControl) {
+//        switch segmentedControlRadius.selectedSegmentIndex {
+//        case 0:
+//            radius = 800
+//        case 1:
+//            radius = 5000
+//        case 2:
+//            radius = 20000
+//        default:
+//            radius = 800
+//        }
+//    }
     
     @IBAction func goButton(sender: UIButton) {
         println("LIFTOFFFFFFF")
